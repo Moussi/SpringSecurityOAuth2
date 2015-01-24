35f4259187312f6fa7b32ec7cc6837f0fa5c3609
Aymen MOUSSI <moussi.gl@gmail.com>
2015-01-24 22:33:31 +0100

    Setup project

---
+++ SpringRestSecurityOauth/src/main/java/com/moussi/services/DataService.java
@@ -0,0 +1,12 @@
+package com.moussi.services;
+
+import java.util.List;
+
+import com.moussi.models.User;
+/**
+ * @author MOUSSI Aymen
+ *
+ */
+public interface DataService {
+	public List<User> getUserList();
+}
